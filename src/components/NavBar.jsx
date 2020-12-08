26328b71a9b9a2868eb3e09c7d3914a95a3090a7
Federico Martinez <fedemartinez86uy@hotmail.com>
2020-12-08 16:25:37 -0300

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var NavBar = React.createClass({
+   render: function(){
+       return(
+           <div>
+               navbar
+           </div>
+       );
+   } 
+});
+
+module.exports = NavBar; 

431982efddf93d3cfe702cecdfc6cc0759570514
Federico Martinez <fedemartinez86uy@hotmail.com>
2020-12-08 19:35:08 -0300

    Menu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Dropdown } from 'react-bootstrap';
+import { DropdownSubmenu} from 'react-bootstrap-submenu';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<Dropdown>
+  <Dropdown.Toggle variant="success" id="dropdown-basic">
+    Menú
+  </Dropdown.Toggle>
+
+  <Dropdown.Menu>
+    <Dropdown.Item href="#/action-1">Opción 1</Dropdown.Item>
+    <Dropdown.Item href="#/action-2">Opción 2</Dropdown.Item>
+    <DropdownSubmenu title='Opción 3'>
+      <Dropdown.Item href="#/action-2">Opción A</Dropdown.Item>
+    </DropdownSubmenu>
+    <Dropdown.Item href="#/action-3">Opción 4</Dropdown.Item>
+  </Dropdown.Menu>
+</Dropdown>
   );
 }
 
